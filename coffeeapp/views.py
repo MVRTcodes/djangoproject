6261eb26ea7db1ec28a3fae5e76234705543fdd0
MVRT <alj.martinez.j@gmail.com>
2025-01-30 15:10:32 +0100

    project and tasks routing

--- coffeeapp/views.py
+++ coffeeapp/views.py
@@ -1,4 +1,6 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def index(request):
@@ -9,3 +11,11 @@ def hello(request, username):
 
 def about(request):
     return HttpResponse('About')
+
+def projects(request):
+    p = list(Project.objects.values())
+    return JsonResponse(p, safe=False)
+
+def tasks(request, id):
+    t = get_object_or_404(Task, id=id)
+    return HttpResponse('Task: %s' % t.title)
