489d056a5f6b4c61e3e2f612807e5d35fa64b448
MVRT <alj.martinez.j@gmail.com>
2025-02-03 19:23:29 +0100

    folder namechange + tasks upgrade

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for coffeesite project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('coffeeapp.urls'))
+    path('', include('projectsapp.urls'))
 ]
--- projectsapp/apps.py
+++ projectsapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoffeeappConfig(AppConfig):
+class ProjectsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coffeeapp'
+    name = 'projectsapp'
